339f7df7846e36c4863d6f0a738dde583ba1be1e
2-ring <107005190+2-ring@users.noreply.github.com>
2025-06-04 23:12:19 +0100

    Moved file

--- src/components/features/Mission.jsx
+++ src/components/features/Mission.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { forwardRef, useEffect, useRef, useState } from "react";
-import StageBanner from "./StageBanner.jsx";
+import StageBanner from "../layout/StageBanner.jsx";
 
 const components = [StageBanner, StageBanner, StageBanner];
 
