24375bc6b66600d9ee663b02355f2ac7c576d6fd
Zach S. <zachstellato@gmail.com>
2025-08-20 19:24:12 -0500

    chore: build

--- dist/assets/Footer-ChAlc8gk.js
+++ dist/assets/Footer-ChAlc8gk.js
@@ -1,4 +1,4 @@
-import{j as e,L as a,S as t}from"./index-B7cPExz9.js";import{c as l,M as r,L as i}from"./mail-1JfTqrB1.js";/**
+import{j as e,L as a,S as t}from"./index-DGkV7JV3.js";import{c as l,M as r,L as i}from"./mail-CP-uthfQ.js";/**
  * @license lucide-react v0.503.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- docs/assets/Team-U6zVzo22.js
+++ docs/assets/Team-U6zVzo22.js
@@ -1,4 +1,4 @@
-import{r as n,j as t,A as J,m as p}from"./index-B7cPExz9.js";import{c as z,M as O,L as T}from"./mail-1JfTqrB1.js";/**
+import{r as n,j as t,A as J,m as p}from"./index-DGkV7JV3.js";import{c as z,M as O,L as T}from"./mail-CP-uthfQ.js";/**
  * @license lucide-react v0.503.0 - ISC
  *
  * This source code is licensed under the ISC license.
